3f2a6d3bbe6f8ca8f9517154a1867456d143e2fa
Matt <141237843+MATT30303@users.noreply.github.com>
2026-07-16 20:47:34 -0300

    feat: added images and icons

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -1,6 +1,18 @@
 export {default as Logo} from "./Logo.png"
 export {default as PromoPotus1} from "./PromoPotus1.png"
-export {default as PotusIcon} from "./PotusIcon.png"
-export {default as SingonioIcon} from "./SingonioIcon.png"
 export {default as MonsteraIcon} from "./MonsteraIcon.png"
 export {default as PromoIcon} from "./PromoIcon.png"
+
+export {default as PotusIcon} from "./potus/PotusIcon.png"
+export {default as PotusComun} from "./potus/PotusComun.png"
+export {default as PotusVariegado} from "./potus/PotusVariegado.png"
+export {default as PotusMarbleQueen} from "./potus/PotusMarbleQueen.png"
+export {default as PotusNjoy} from "./potus/PotusNjoy.png"
+export {default as PotusLemon} from "./potus/PotusLemon.png"
+
+export {default as SingonioIcon} from "./singonio/SingonioIcon.png"
+export {default as SingonioComun} from './singonio/SingonioComun.png'
+export {default as SingonioPuntaDeFlecha} from './singonio/SingonioPuntaDeFlecha.png'
+export {default as SingonioVariegado} from './singonio/SingonioVariegado.png'
+export {default as SingonioMaria} from './singonio/SingonioMaria.png'
+export {default as SingonioRosado} from './singonio/SingonioRosado.png'
