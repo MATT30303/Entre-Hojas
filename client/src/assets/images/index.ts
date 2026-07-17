69f3bc780b5edfa86fc4d3fdfba9f83aa1d89fb8
Matt <141237843+MATT30303@users.noreply.github.com>
2026-07-16 21:37:11 -0300

    feat: added images and conversions to webp

--- client/src/assets/images/index.ts
+++ client/src/assets/images/index.ts
@@ -1,18 +1,35 @@
 export {default as Logo} from "./Logo.png"
 export {default as PromoPotus1} from "./PromoPotus1.png"
-export {default as MonsteraIcon} from "./MonsteraIcon.png"
 export {default as PromoIcon} from "./PromoIcon.png"
 
-export {default as PotusIcon} from "./potus/PotusIcon.png"
-export {default as PotusComun} from "./potus/PotusComun.png"
-export {default as PotusVariegado} from "./potus/PotusVariegado.png"
-export {default as PotusMarbleQueen} from "./potus/PotusMarbleQueen.png"
-export {default as PotusNjoy} from "./potus/PotusNjoy.png"
-export {default as PotusLemon} from "./potus/PotusLemon.png"
+export {default as AnturioIcon} from "./anturio/AnturioIcon.webp"
+export {default as AnturioRojo} from "./anturio/AnturioRojo.webp"
 
-export {default as SingonioIcon} from "./singonio/SingonioIcon.png"
-export {default as SingonioComun} from './singonio/SingonioComun.png'
-export {default as SingonioPuntaDeFlecha} from './singonio/SingonioPuntaDeFlecha.png'
-export {default as SingonioVariegado} from './singonio/SingonioVariegado.png'
-export {default as SingonioMaria} from './singonio/SingonioMaria.png'
-export {default as SingonioRosado} from './singonio/SingonioRosado.png'
+export {default as CalatheaIcon} from "./calathea/CalatheaIcon.webp"
+export {default as CalatheaTriostar} from "./calathea/CalatheaTriostar.webp"
+
+export {default as MarantaIcon} from "./maranta/MarantaIcon.webp"
+export {default as MarantaLeuconeura} from "./maranta/MarantaLeuconeura.webp"
+
+export {default as MonsteraIcon} from "./monstera/MonsteraIcon.webp"
+export {default as MonsteraAdansonii} from "./monstera/MonsteraAdansonii.webp"
+export {default as MonsteraDeliciosa} from "./monstera/MonsteraDeliciosa.webp"
+
+export {default as Philodentro} from "./philodentro/PhilodentronIcon.webp"
+
+export {default as PotusIcon} from "./potus/PotusIcon.webp"
+export {default as PotusComun} from "./potus/PotusComun.webp"
+export {default as PotusVariegado} from "./potus/PotusVariegado.webp"
+export {default as PotusMarbleQueen} from "./potus/PotusMarbleQueen.webp"
+export {default as PotusNjoy} from "./potus/PotusNjoy.webp"
+export {default as PotusLemon} from "./potus/PotusLemon.webp"
+
+export {default as SingonioIcon} from "./singonio/SingonioIcon.webp"
+export {default as SingonioComun} from './singonio/SingonioComun.webp'
+export {default as SingonioPuntaDeFlecha} from './singonio/SingonioPuntaDeFlecha.webp'
+export {default as SingonioVariegado} from './singonio/SingonioVariegado.webp'
+export {default as SingonioMaria} from './singonio/SingonioMaria.webp'
+export {default as SingonioRosado} from './singonio/SingonioRosado.webp'
+
+
+
